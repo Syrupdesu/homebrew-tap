f9fd1c57f6c9b54d82123de78c023386652a5d78
Syrupdesu <122212359+Syrupdesu@users.noreply.github.com>
2026-03-04 08:49:25 -0700

    初始提交

---
+++ Casks/playcover-community@nightly.rb
@@ -0,0 +1,27 @@
+cask "playcover-community@nightly" do
+  version "1409"
+  sha256 :no_check
+
+  url "https://nightly.link/PlayCover/PlayCover/workflows/2.nightly_release/develop/PlayCover_nightly_#{version}.dmg.zip"
+  name "PlayCover Community (Nightly)"
+  desc "Sideload iOS apps and games on Apple Silicon"
+  homepage "https://github.com/PlayCover/PlayCover"
+
+  depends_on arch: :arm64
+  depends_on macos: ">= :monterey"
+
+  conflicts_with cask: [
+    "playcover-community",
+    "playcover-community@beta",
+  ]
+  app "PlayCover.app"
+
+  zap trash: [
+    "~/Library/Application Support/io.playcover.PlayCover",
+    "~/Library/Caches/io.playcover.PlayCover",
+    "~/Library/Containers/io.playcover.PlayCover",
+    "~/Library/Frameworks/PlayTools.framework",
+    "~/Library/Preferences/io.playcover.PlayCover.plist",
+    "~/Library/Saved Application State/io.playcover.PlayCover.savedState",
+  ]
+end
