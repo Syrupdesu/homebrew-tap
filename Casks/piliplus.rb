8d6ab22e48a79d867e91413eb21d07520c406bdc
Syrupdesu <122212359+Syrupdesu@users.noreply.github.com>
2026-03-15 13:42:14 -0600

    Update URL

--- Casks/piliplus.rb
+++ Casks/piliplus.rb
@@ -1,8 +1,8 @@
 cask "piliplus" do
-  version "2.0.1.1"
+  version "2.0.1.1,2.0.1,4775"
   sha256 :no_check
 
-  url "https://github.com/bggRGjQaUbCoE/PiliPlus/releases/download/#{version}/PiliPlus_macos_2.0.0+4775.dmg"
+  url "https://github.com/bggRGjQaUbCoE/PiliPlus/releases/download/#{version.csv.first}/PiliPlus_macos_#{version.csv.second}+#{version.csv.third}.dmg"
   name "PiliPlus"
   desc "Third-party bilibili client developed with Flutter"
   homepage "https://github.com/bggRGjQaUbCoE/PiliPlus"
