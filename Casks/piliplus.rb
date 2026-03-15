ab4f4defdbcad6da3c51d7e3c0aa7a2b8d01ee13
Syrupdesu <122212359+Syrupdesu@users.noreply.github.com>
2026-03-15 13:28:29 -0600

    Update to 2.0.1.1

--- Casks/piliplus.rb
+++ Casks/piliplus.rb
@@ -1,5 +1,5 @@
 cask "piliplus" do
-  version "2.0.0.1"
+  version "2.0.1.1"
   sha256 :no_check
 
   url "https://github.com/bggRGjQaUbCoE/PiliPlus/releases/download/#{version}/PiliPlus_macos_2.0.0+4721.dmg"
