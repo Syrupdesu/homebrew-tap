07731af2cd00747a3d633970950063f8573d7708
Syrupdesu <122212359+Syrupdesu@users.noreply.github.com>
2026-03-16 16:10:33 -0600

    Add koharu 0.38.0

---
+++ Casks/koharu.rb
@@ -0,0 +1,11 @@
+cask "koharu" do
+  version "0.38.0"
+  sha256 :no_check
+
+  url "https://github.com/mayocream/koharu/releases/download/#{version}/koharu_#{version}_aarch64.dmg"
+  name "Koharu"
+  desc "ML-powered manga translator, written in Rust."
+  homepage "https://github.com/mayocream/koharu"
+
+  app "koharu.app" 
+end
