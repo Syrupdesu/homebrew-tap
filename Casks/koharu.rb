9d4471b2e689c0f5dfbc4618476bd20bda2cce77
Syrupdesu <122212359+Syrupdesu@users.noreply.github.com>
2026-03-18 22:39:02 -0600

    Update to 0.38.1

--- Casks/koharu.rb
+++ Casks/koharu.rb
@@ -1,5 +1,5 @@
 cask "koharu" do
-  version "0.38.0"
+  version "0.38.1"
   sha256 :no_check
 
   url "https://github.com/mayocream/koharu/releases/download/#{version}/koharu_#{version}_aarch64.dmg"
