000a79faebd9daacf846fbf8fb658f51623e62d4
Syrupdesu <122212359+Syrupdesu@users.noreply.github.com>
2026-04-21 14:58:49 -0600

    Update to 1462

--- Casks/playcover-community@nightly.rb
+++ Casks/playcover-community@nightly.rb
@@ -1,5 +1,5 @@
 cask "playcover-community@nightly" do
-  version "1456"
+  version "1462"
   sha256 :no_check
 
   url "https://nightly.link/PlayCover/PlayCover/workflows/2.nightly_release/develop/PlayCover_nightly_#{version}.dmg.zip",
