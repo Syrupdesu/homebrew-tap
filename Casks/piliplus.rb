e04be3356916c783fea877c17bcb186a146bd3f0
Syrupdesu <122212359+Syrupdesu@users.noreply.github.com>
2026-04-21 19:52:44 -0600

    Update sha256

--- Casks/piliplus.rb
+++ Casks/piliplus.rb
@@ -1,6 +1,6 @@
 cask "piliplus" do
   version "2.0.4,2.0.4,4848"
-  sha256 :no_check
+  sha256 "03fdbbf9e3630ae589e061a54e1e1ef9f5369a95cc93d7ee9b9c458d8ec20d5b"
 
   url "https://github.com/bggRGjQaUbCoE/PiliPlus/releases/download/#{version.csv.first}/PiliPlus_macos_#{version.csv.second}+#{version.csv.third}.dmg"
   name "PiliPlus"
