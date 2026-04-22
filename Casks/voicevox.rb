6764327de5ebe6a95844b647a1ed25248e537b54
Syrupdesu <122212359+Syrupdesu@users.noreply.github.com>
2026-04-22 12:07:40 -0600

    Fix style

--- Casks/voicevox.rb
+++ Casks/voicevox.rb
@@ -2,19 +2,20 @@ cask "voicevox" do
   version "0.25.1"
 
   on_arm do
+    sha256 "a1501e26a5759f5cf7ec571bb82a33c227e61ee66646f8e17c3504441085d99d"
+
     url "https://github.com/VOICEVOX/voicevox/releases/download/#{version}/voicevox-macos-cpu-arm64-#{version}.zip",
         verified: "github.com/VOICEVOX/voicevox/"
-    sha256 "a1501e26a5759f5cf7ec571bb82a33c227e61ee66646f8e17c3504441085d99d"
   end
-
   on_intel do
+    sha256 "9a7ff1391b85ec57c070128a137a3010c44d3b376487272d36f479f28ca20289"
+
     url "https://github.com/VOICEVOX/voicevox/releases/download/#{version}/voicevox-macos-cpu-x64-#{version}.zip",
         verified: "github.com/VOICEVOX/voicevox/"
-    sha256 "9a7ff1391b85ec57c070128a137a3010c44d3b376487272d36f479f28ca20289"
   end
 
   name "VOICEVOX"
-  desc "A free Japanese text-to-speech software"
+  desc "Free Japanese text-to-speech software"
   homepage "https://voicevox.hiroshiba.jp/"
 
   livecheck do
