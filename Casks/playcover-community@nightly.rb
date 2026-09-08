0b92cdf96efdad5b5d656cc519dd88a5e263c35b
Syrupdesu <122212359+Syrupdesu@users.noreply.github.com>
2026-09-08 10:48:56 -0600

    更新SHA256值

--- Casks/playcover-community@nightly.rb
+++ Casks/playcover-community@nightly.rb
@@ -1,6 +1,6 @@
 cask "playcover-community@nightly" do
   version "1602"
-  sha256 "1058b5b568b3c9f8d64ca7e249fc507b02f6787f82ebd4274b568b170da5f089"
+  sha256 "3b47b8cd639048fd454124e5ee38eda79d26173bbddfa551598d0ddb6d94ce48"
 
   url "https://nightly.link/PlayCover/PlayCover/workflows/2.nightly_release/develop/PlayCover_nightly_#{version}.dmg.zip"
   name "PlayCover Community (Nightly)"
