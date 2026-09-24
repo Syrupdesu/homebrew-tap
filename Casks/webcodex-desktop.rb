adf7ec056555eb63b27ff4442ea562134c1dc122
Syrupdesu <122212359+Syrupdesu@users.noreply.github.com>
2026-09-23 20:26:33 -0600

    Update zap trash

--- Casks/webcodex-desktop.rb
+++ Casks/webcodex-desktop.rb
@@ -20,9 +20,9 @@ cask "webcodex-desktop" do
   app "WebCodex Desktop.app"
 
   zap trash: [
-    "~/Library/Application Support/WebCodex Desktop",
-    "~/Library/Logs/WebCodex Desktop",
-    "~/Library/Preferences/dev.webcodex.desktop.plist",
-    "~/Library/Saved Application State/dev.webcodex.desktop.savedState",
-  ]
+       "~/Library/Application Support/dev.webcodex.desktop",
+       "~/Library/Caches/dev.webcodex.desktop",
+       "~/Library/Preferences/dev.webcodex.desktop.plist",
+       "~/Library/WebKit/dev.webcodex.desktop",
+     ]
 end
